e234fb5358d16aa61443783e60541a8128dbe7e1
Rafael Giacomin <faellg@gmail.com>
2020-10-01 14:23:53 -0300

    more bases to commit

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,13 +8,13 @@ User.destroy_all
 
 manager1 = User.create!(email: "manager1@test.com", password: '123456', name: Faker::Name.name, cpf: Faker::CPF.numeric, manager: true )
 
-base = Base.create!(name: "Banco de Dados de Síndrome Respiratória Aguda Grave", description: "Vigilância de Síndrome Respiratória Aguda Grave (SRAG) O Ministério da Saúde, por meio da Secretaria de Vigilância em Saúde (SVS), desenvolve a vigilância da Síndrome Respiratória Aguda...", legal_base: "Lei 1111/2019", technical_area: "Secretaria de Vigilancia Sanitária", user: manager1)
+base = Base.create!(name: "Banco de Dados de Síndrome Respiratória Aguda Grave", description: "Vigilância de Síndrome Respiratória Aguda Grave (SRAG) O Ministério da Saúde, por meio da Secretaria de Vigilância em Saúde (SVS), desenvolve a vigilância da Síndrome Respiratória Aguda...", legal_base: "Cumprimento das obrigações legais ou regulatórias, Execução de políticas públicas, Proteção da vida, Tutela da saúde, Condução de estudos e pesquisas", technical_area: "Secretaria de Vigilancia Sanitária", user: manager1)
 
 csv_options = { col_sep: ';', quote_char: '"', headers: :first_row }
 csv_text = "./lib/seeds/srag_hospitalizado.csv"
 
 CSV.foreach(csv_text, csv_options) do |row|
-  variable = Variable.create!(name: row[0], description: row[1], category: row[2], sensibility: row[3], base: base)
+  variable = Variable.create!(name: row[0], description: row[1], category: row[2], base: base)
 end
 
 manager2 = User.create!(email: "manager2@test.com", password: '123456', name: Faker::Name.name, cpf: Faker::CPF.numeric, manager: true )
