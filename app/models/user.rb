192191ad7208e87e6ab8d890b1cfbe876898c199
Marcos Camara <marcos.tc@gmail.com>
2020-10-05 12:56:06 -0300

    Correção de user.rb devido a erro de pluralização da classe Base.

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,7 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  has_many :bases
+  has_many :bases, class_name: 'Base'  # rails admin pluralizou errado e nao achou a classe
   has_many :orders, dependent: :destroy
 
   has_one_attached :photo
