cf903c393217d5071ee1e45b2d7b812520339bbc
Rafael Giacomin <faellg@gmail.com>
2020-10-07 10:14:04 -0300

    funcionando show para usuário ainda não logado e consequente pedido de base

--- app/controllers/bases_controller.rb
+++ app/controllers/bases_controller.rb
@@ -1,5 +1,5 @@
 class BasesController < ApplicationController
-  skip_before_action :authenticate_user!, only: [:index]
+  skip_before_action :authenticate_user!, only: [:index, :show]
   before_action :set_base, only: [:show, :edit, :update, :destroy]
 
   def index
