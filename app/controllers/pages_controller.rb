d32b1ab591487a6fbed03b628cddd6455604ad41
Fabiana Silva <fabibarreiros@gmail.com>
2020-10-07 11:03:39 -0300

    adicionada a página about us

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,5 +1,9 @@
 class PagesController < ApplicationController
-  skip_before_action :authenticate_user!, only: :home
+  skip_before_action :authenticate_user!, only: [:home, :about_us]
+
   def home
+  end
+
+  def about_us
   end
 end
